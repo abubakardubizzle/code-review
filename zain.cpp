f5cd647b42a64450fc3f5101600ff308f437478d
MZainShaheen <zain.shaheen@dubizzlelabs.com>
2023-08-08 11:32:53 +0500

    Revert "checking cherry-pick feature"
    This reverts commit f7a3ecf17adc66652b5fcefde10e8e681fff8a0d.

--- zain.cpp
+++ zain.cpp
@@ -6,7 +6,7 @@ int main(){
     int target=6;
     int size = 10;
     for (int i = 0; i<size-1; i++){
-        for (int j=i+1;j<size;j++){
+        for (int j=i+1;j<size-1;j++){
             if (arr[i]+arr[j]==target){
                 cout<<i<<" "<<j;
                 return 0;
